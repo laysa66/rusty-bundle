1ea59abcf6089ca2bc9cf9077a8d1ba25b06cef6
Syrine Benali <benalisyrine8@gmail.com>
2026-03-19 14:01:57 +0100

    Add protobuf and protobuf-json-mapping dependencies; implement send_bundle function for message serialization and TCP communication

--- src/network/client.rs
+++ src/network/client.rs
@@ -1,5 +1,7 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use crate::protobuf::bundle_proto::Bundle as ProtobufBundle;
+use crate::routing::model::Node;
 
 /// Connects to a peer node and sends a message
 pub fn send_message(address: &str, message: &str) {
@@ -22,4 +24,53 @@ pub fn send_message(address: &str, message: &str) {
             println!("[Client] Could not connect to {}: {}", address, e);
         }
     }
+}
+
+pub fn send_bundle(bundle: ProtobufBundle, peers: Vec<Node>) {
+
+    // convert the bundle into the protobuf generated bundle
+    //serialization of the protobuf to JSON string using protobuf-json-mapping
+    let payload = match protobuf_json_mapping::print_to_string(&bundle) {
+        Ok(json) => json.into_bytes(),
+        Err(e) => {
+            eprintln!("send_bundle failed to serialize bundle {} : {}", bundle.id, e);
+            return;
+        }
+    };
+
+    //iterate over eachh peer that the routing engine decided on
+    for peer in peers {
+        
+        //build the peer address from the ip and port
+        let address = format!("{}:{}", peer.address, peer.port);
+
+        //Open a direct TCP connection to the peer
+        let mut stream = match TcpStream::connect(address) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("send_bundle TCP connect to {} failed: {}", address, e);
+                return;
+            }
+        };
+
+        //sending with length prefix to let the receiver know exactly how many bytes to read
+        let len = payload.len() as u32;
+        if let Err(e) = stream
+            .write_all(&len.to_be_bytes()) //writing the entire buffer to the tcp stream
+            .and_then(|_| stream.write_all(&payload)) // this only runs if the previous is Ok
+        {
+            eprintln!("send_bundle failed to write to {}: {}", address, e);
+            return;
+        }
+
+        //waiting for the ack peers
+        let mut ack = [0u8; 4]; //buffer allocation
+        match stream.read_exact(&mut ack) { //reads exactly 4 bytes from tcp stream into the ack buffer
+            Ok(_) if &ack == b"ack\n" => {}
+            Ok(_) => eprintln!("[send_bundle] unexpected ack from {}: {:?}", address, ack),
+            Err(e) => eprintln!("[send_bundle] failed to read ack from {}: {}", address, e),
+        }
+
+    }
+
 }
