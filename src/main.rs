2f396ba6b34c6b40db34bdd0f173ec2356ca5e91
Syrine Benali <benalisyrine8@gmail.com>
2026-03-30 18:47:52 +0200

    Add storage capacity parameter to JsonFileStorage initialization in the main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use storage::JsonFileStorage;
 
 fn main() {
     // creer le,ficheir de storage automatiquement au lancer de l'app
-    let storage = Box ::new(JsonFileStorage ::new("./bundles".to_string()));
+    let storage = Box ::new(JsonFileStorage ::new("./bundles".to_string(), 10));
 
 }
